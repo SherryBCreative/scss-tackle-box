159936e33c295c6c311efa0803174e0f0aac4f78
SherryBCreative <SherryBCreative@users.noreply.github.com>
2018-01-07 05:07:06 -0600

    0.0.9

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,18 @@
 # Change Log
 
+## [0.0.9] - 2018-01-07
+
+### Added
+
+- Group breakpoint mixins with placeholders
+- various group placeholders
+- `@mixin tb-map-deep-set()`
+
+### Removed
+
+- grunt-scss-lint
+- time-grunt
+
 ## [0.0.8] - 2017-09-20
 
 ### Added
--- Gruntfile.js
+++ Gruntfile.js
@@ -1,47 +1,59 @@
 module.exports = function ( grunt ) {
   'use strict';
 
-  require( 'time-grunt' )( grunt );
 
-  require( 'jit-grunt' )(
-    grunt, {
-      scsslint: 'grunt-scss-lint'
-    }
-  );
+  require( 'jit-grunt' )( grunt );
 
-  grunt.initConfig(
-    {
+  grunt.initConfig( {
+                      pkg: grunt.file.readJSON( 'package.json' ),
 
-      pkg: grunt.file.readJSON( 'package.json' ),
+                      sass: {
+                        options: { outputStyle: 'expanded' },
 
-      scsslint: {
-        report: {
-          options: {
-            config:         '.scss-lint.yml',
-            reporterOutput: null,
-          },
-          files:   [
-            {
-              src: [
-                '_tackle-box.scss', 'tackle/*.scss',
-              ]
-            }
-          ]
-        }
-      },
+                        testing: {
+                          files: [
+                            {
+                              expand: false,
+                              src:    [
+                                'tackle-box.scss',
+                              ],
+                              dest:   'test.css',
+                            }
+                          ],
+                        },
+                      },
 
-      watch: {
-        scss: {
-          files: [
-            '**/*.scss'
-          ],
-          tasks: [
-            'scsslint'
-          ]
-        }
-      }
-    }
-  );
+                      scsslint: {
+                        report: {
+                          options: {
+                            config:         '.scss-lint.yml',
+                            reporterOutput: null,
+                          },
+                          files:   [
+                            {
+                              src: [
+                                '_tackle-box.scss', 'tackle/*.scss',
+                              ]
+                            }
+                          ]
+                        }
+                      },
+
+                      watch: {
+                        scss: {
+                          files: [
+                            '**/*.scss'
+                          ],
+                          tasks: [
+                            'testSCSS'
+                          ]
+                        }
+                      }
+                    } );
+
+  grunt.registerTask( 'testSCSS', [
+    'sass', //
+  ] );
 
   grunt.registerTask( 'default', [ 'watch' ] );
 };
